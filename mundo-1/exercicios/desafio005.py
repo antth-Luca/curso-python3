75104269d73e5b9c5c005b0ec1a8308864506d9f
Luca Anthony <lucamafort05@gmail.com>
2023-02-17 12:42:55 -0300

    Aula 07
    Desafios

--- mundo-1/exercicios/desafio005.py
+++ mundo-1/exercicios/desafio005.py
@@ -1,4 +1,4 @@
 n = int(input('Digite um número: '))
-ant = n-1
-suc = n+1
+ant = n - 1
+suc = n + 1
 print('O antecessor de {} é {} e o seu sucessor é {}'.format(n, ant, suc))
---
+++ mundo-1/exercicios/desafio008.py
@@ -0,0 +1,4 @@
+m = float(input('Quantos metros temos? '))
+cm = m*100
+mm = cm*10
+print('{} metros é a mesma coisa que {} centímetros ou {} milímetros'.format(m, cm, mm))
