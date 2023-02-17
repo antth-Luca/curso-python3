8e510722de043eebb82b328bbcb05392b13cebf6
Luca Anthony <lucamafort05@gmail.com>
2023-02-17 09:43:45 -0300

    Create 003.py

---
+++ mundo-1/exercicios/003.py
@@ -0,0 +1,6 @@
+nome = input('Olá! Qual o seu nome? ')
+print('{} vamos somar?!'.format(nome))
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro: '))
+s = n1+n2
+print('A soma entre {} e {} é {}'.format(n1, n2, s))
