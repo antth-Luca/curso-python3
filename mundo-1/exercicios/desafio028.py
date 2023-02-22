c0f03c18b2a088de43451550df952d3da7878ddf
Luca Anthony <lucamafort05@gmail.com>
2023-02-22 12:51:19 -0300

    correção

--- mundo-1/exercicios/desafio028.py
+++ mundo-1/exercicios/desafio028.py
@@ -1,10 +1,9 @@
-from random import choice
+from random import randint
 print('{:=^56}'.format('Jogo da Adivinhação'))
-lista = [1, 2, 3, 4, 5]
-nEscolhido = choice(lista)
+nEscolhido = randint(0,5)
 print('Estou pensando em um número de 0 a 5.', end=' ')
 nDigitado = int(input('Tente adivinhar: '))
 if nDigitado == nEscolhido:
     print('Muito bem você acertou!')
 else:
-    print('Errado! Que peninha, você pode tentar de novo...')
+    print('Errado! Eu pensei em {}, você pode tentar de novo...'.format(nEscolhido))
--- mundo-1/exercicios/desafio032.py
+++ mundo-1/exercicios/desafio032.py
@@ -1,6 +1,9 @@
+from datetime import date
 print('{:=^40}'.format('Esse ano é Bissexto?'))
-ano = int(input('Digite o ano: '))
-if ano % 4 == 0:
+ano = int(input('Digite o ano ou 0 (zero) para o ano atual: '))
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('O ano de {} é bissexto!'.format(ano))
 else:
     print('O ano de {} não é bissexto.'.format(ano))
